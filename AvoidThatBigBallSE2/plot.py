b26ade1d719c15e77b7d59274ae87f7eb87a1751
void32 <mortenalbecknielsen@gmail.com>
2015-01-17 18:42:30 +0100

    fatted up the plot script

--- AvoidThatBigBallSE2/plot.py
+++ AvoidThatBigBallSE2/plot.py
@@ -1,9 +1,21 @@
 from mpl_toolkits.mplot3d import Axes3D
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-data = numpy.loadtxt('/tmp/path.txt')
-fig = plt.figure()
-ax = fig.gca() #projection='2d'
-ax.plot(data[:,0],data[:,1],'.-')
-plt.show()
 
+#setup figure
+figure = plt.figure('Plot of path')
+figure.gca().set_aspect('equal')
+figure.gca().set_xlim(-1, 1)
+figure.gca().set_ylim(-1, 1)
+
+#plot data
+data = np.loadtxt('/tmp/path.txt')
+ax = figure.gca() #projection='2d'
+ax.plot(data[:,0],data[:,1],'.-')
+
+#plot ball
+circle=plt.Circle((0.0,0.0),0.7,fc=np.random.random(3),picker=True, alpha=0.5)
+figure.gca().add_patch(circle)
+
+#Display the plot
+plt.show(block=True) #keep the window open
